5533e3ac2a4993a01c0dbbd6e334cfdd2d5d0929
Karthik S <7162945-karthik61@users.noreply.gitlab.com>
2021-07-21 12:06:07 +0530

    build fix

--- vue.config.js
+++ vue.config.js
@@ -1,10 +1,17 @@
+const path = require('path')
 module.exports = {
 
   configureWebpack: {
     optimization: {
       splitChunks: false
-    }
+    },
+    output: {
+      path: path.resolve(__dirname, "./dist"),
+      filename: 'js/build.js',
+      publicPath: '/',
+    }, 
   },
+ 
   css: {
     extract: false,
   },
